48a756ba25ddc5965cf6822e7bf6ebe9f8cbd825
Your Name <you@example.com>
2026-07-27 14:50:16 -0400

    add header and files

---
+++ include/sort.h
@@ -0,0 +1,24 @@
+#ifndef BASICS_SORT_H
+#define BASICS_SORT_H
+
+#include <vector>
+
+struct Node {
+    int value;
+    Node* next;
+};
+
+class LinkedList {
+public:
+    Node* head = nullptr;
+    int size = 0;
+
+    ~LinkedList();
+    void push_front(int v);
+    static LinkedList from_vector(const std::vector<int>& v);
+};
+
+void sort_insertion(LinkedList& list); //O(n^2)
+void sort_merge(LinkedList& list); //O(n log n)
+
+#endif //BASICS_SORT_H
